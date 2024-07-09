efab83b39838f025fa1db98628a06832e307a02c
Samir Ahmad <samir.ahmad@medicus.ai>
2024-07-09 20:14:03 +0400

    Add user identity to db context

---
+++ DataAccess/Entity/AppUser.cs
@@ -0,0 +1,20 @@
+
+
+
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+using ReminderApp.DataAccess.types;
+
+namespace ReminderApp.DataAccess.Entity;
+
+public class AppUser : IdentityUser<Guid>
+{
+    [Required()]
+    public required string FirstName { get; set; }
+    [Required()]
+    public required string LastName { get; set; }
+
+    [EnumDataType(typeof(UserRole))]
+    [Required()]
+    public required int Role { get; set; }
+}
